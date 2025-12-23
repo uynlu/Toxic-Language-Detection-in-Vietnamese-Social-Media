a403bc64151dc01377d1e270255c5bfc5371fb1d
uynlu <uyenluubao2004@gmail.com>
2025-12-23 18:58:06 +0700

    Update error utils

--- model_running/model_utils/evaluation_errors_utils.py
+++ model_running/model_utils/evaluation_errors_utils.py
@@ -7,11 +7,14 @@ from sklearn.metrics import (
 
 
 def error(labels, predictions, num_labels=2):
+    acc = accuracy_score(labels, predictions)
     if num_labels == 2:
         f1 = f1_score(labels, predictions)
+        precision = precision_score(labels, predictions)
+        recall = recall_score(labels, predictions)
     else:
         f1 = f1_score(labels, predictions, average="macro")
-    acc = accuracy_score(labels, predictions)
-    precision = precision_score(labels, predictions)
-    recall = recall_score(labels, predictions)
+        precision = precision_score(labels, predictions, average="macro")
+        recall = recall_score(labels, predictions, average="macro")
+    
     return acc, f1, precision, recall
